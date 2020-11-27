b9da9d9d1d632960063f0a5bc292b4c49891e577
Artem Sukhov <suhov@iypt.ru>
2020-11-27 14:46:32 +0300

    Updated button classes Protected names and, virtual press function. Button manager is addable by buttons now.

--- Buttons.h
+++ Buttons.h
@@ -7,39 +7,39 @@ typedef void(*Action) ();
 struct Button {
 public:
 	Button(const char* name, Point position, Vector size, Action func) :
-		name(name),
-		position(position),
-		func(func)
+		name_(name),
+		position_(position),
+		func_(func)
 	{
-		this->size.equalize(size);
+		this->size_.equalize(size);
 	}
 
 	virtual void drawButton() {
-		RECT rect{ position.x,				 position.y,
-				   position.x + size.getX(), position.y + size.getY() };
+		RECT rect{ position_.x,				   position_.y,
+				   position_.x + size_.getX(), position_.y + size_.getY() };
 
 		txSetFillColor (TX_WHITE);
 		txRectangle	   (rect.left, rect.bottom, rect.right, rect.top);
 		txSetColor	   (TX_BLACK);
-		txDrawText	   (rect.left, rect.bottom, rect.right, rect.top, name);
+		txDrawText	   (rect.left, rect.bottom, rect.right, rect.top, name_);
 	}
 
 	virtual bool isPressed(float x, float y) {
-		float eqX = x - position.x;
-		float eqY = y - position.y;
+		float eqX = x - position_.x;
+		float eqY = y - position_.y;
 
-		return eqX > 0 && eqY > 0 && eqX < size.getX() && eqY < size.getX();
+		return eqX > 0 && eqY > 0 && eqX < size_.getX() && eqY < size_.getX();
 	}
 
-	void press() {
-		func();
+	virtual void press() {
+		func_();
 	}
 
 protected:
-	const char* name;
-	Point       position;
-	Vector		size;
-	Action		func;
+	const char* name_;
+	Point       position_;
+	Vector		size_;
+	Action		func_;
 };
 
 struct CircleButton : public Button {
@@ -49,18 +49,18 @@ struct CircleButton : public Button {
 	}
 
 	virtual void drawButton() override {
-		RECT rect{ position.x,				 position.y,
-				   position.x + size.getX(), position.y + size.getY() };
+		RECT rect{ position_.x,				   position_.y,
+				   position_.x + size_.getX(), position_.y + size_.getY() };
 
 		txSetFillColor (TX_WHITE);
-		txCircle	   (position.x + size.getX() / 2, position.y + size.getY() / 2, size.getX() / 2);
+		txCircle	   (position_.x + size_.getX() / 2, position_.y + size_.getY() / 2, size_.getX() / 2);
 		txSetColor	   (TX_BLACK);
-		txDrawText	   (rect.left, rect.bottom, rect.right, rect.top, name);
+		txDrawText	   (rect.left, rect.bottom, rect.right, rect.top, name_);
 	}
 
 	virtual bool isPressed(float x, float y) override {
-		Vector mouseToCenter(position.x + size.getX() / 2 - x, position.y + size.getY() / 2 - y);
-		return mouseToCenter.length() <= size.getX() / 2;
+		Vector mouseToCenter(position_.x + size_.getX() / 2 - x, position_.y + size_.getY() / 2 - y);
+		return mouseToCenter.length() <= size_.getX() / 2;
 	}
 };
 
@@ -71,18 +71,18 @@ struct OvalButton : public Button {
 	}
 
 	virtual void drawButton() override {
-		RECT rect{ position.x - 10,				 position.y,
-				   position.x + size.getX() + 10, position.y + size.getY() };
+		RECT rect{ position_.x - 10,				 position_.y,
+				   position_.x + size_.getX() + 10,  position_.y + size_.getY() };
 
 		txSetFillColor (TX_WHITE);
 		txEllipse      (rect.left, rect.bottom, rect.right, rect.top);
 		txSetColor     (TX_BLACK);
-		txDrawText	   (rect.left, rect.bottom, rect.right, rect.top, name);
+		txDrawText	   (rect.left, rect.bottom, rect.right, rect.top, name_);
 	}
 
 	virtual bool isPressed(float x, float y) override {
-		float firstLength  = (x - position.x) * (x - position.x) / ((size.getX() + 10) * (size.getX() + 10));
-		float secondLength = (y - position.y) * (y - position.y) / (size.getY() * size.getY());
+		float firstLength  = (x - position_.x) * (x - position_.x) / ((size_.getX() + 10) * (size_.getX() + 10));
+		float secondLength = (y - position_.y) * (y - position_.y) / (size_.getY() * size_.getY());
 		return firstLength + secondLength <= 1;
 	}
 };
@@ -95,8 +95,8 @@ struct CloseButton : public Button {
 	}
 
 	virtual void drawButton() override {
-		RECT rect{ position.x,				 position.y,
-				   position.x + size.getX(), position.y + size.getY() };
+		RECT rect{ position_.x,				   position_.y,
+				   position_.x + size_.getX(), position_.y + size_.getY() };
 
 		txSetColor (TX_WHITE);
 		txLine     (rect.left, rect.bottom, rect.right, rect.top);
@@ -107,26 +107,45 @@ struct CloseButton : public Button {
 
 class ButtonManager {
 public:
-	ButtonManager(Button** buttons, int count) {
-		this->count = count;
-		this->buttons = buttons;
+	ButtonManager(Button** buttons, int count) :
+	count_(count)
+	{
+		if (count < maxSize_) {
+			for (int i = 0; i < count; i++) {
+				buttons_[i] = buttons[i];
+			}	
+		}
+		else {
+			std::cerr << "Manager is full";
+		}
+	}
+
+	void addButton(Button* button) {
+		if (count_ < maxSize_) {
+			buttons_[count_] = button;
+			count_++;
+		}
+		else {
+			std::cerr << "Manager is full";
+		}
 	}
 
 	void draw() {
-		for (int i = 0; i < count; i++) {
-			buttons[i]->drawButton();
+		for (int i = 0; i < count_; i++) {
+			buttons_[i]->drawButton();
 		}
 	}
 
 	void press() {
-		for (int i = 0; i < count; i++) {
-			if (buttons[i]->isPressed(txMouseX(), txMouseY())) {
-				buttons[i]->press();
+		for (int i = 0; i < count_; i++) {
+			if (buttons_[i]->isPressed(txMouseX(), txMouseY())) {
+				buttons_[i]->press();
 			}
 		}
 	}
 
-private:
-	Button** buttons;
-	int		 count;
+protected:
+	static const int maxSize_ = 50;
+	Button*			 buttons_[maxSize_];
+	int				 count_;
 };
--- Main.cpp
+++ Main.cpp
@@ -27,33 +27,34 @@ void drawTan() {
 }
 
 void clear() {
-	txSetFillColor(TX_BLACK);
-	txClear();
-	txSetColor(TX_WHITE);
+	txSetFillColor ( TX_BLACK );
+	txClear		   (		  );
+	txSetColor     ( TX_WHITE );
+
 	GlobalCoord->drawAxes();
 }
 
 int main()
 {
-	Vector size (700, 400);
-	Vector xStep (1, 0);
-	Vector yStep (0, 1);
-	Point  zeroOfAxis { 50, 650 };
-	Vector shift (50, 50);
+	Vector size       ( 700, 400 );
+	Vector xStep	  ( 1,   0   );
+	Vector yStep      ( 0,   1   );
+	Point  zeroOfAxis { 50,  650 };
+	Vector shift      ( 50,  50  );
 
 	Video_memory = txVideoMemory();
 
-	CoordSys coordSys (size, xStep, yStep, zeroOfAxis, shift, Vector(0.002, 150));
+	CoordSys coordSys ( size, xStep, yStep, zeroOfAxis, shift, Vector(0.002, 150) );
 	GlobalCoord = &coordSys;
 
 	txSetColor(TX_WHITE);
 	GlobalCoord->drawAxes();
 
 	const int count = 4;
-	Button		 sinButton ("SIN",   Point{ 100, 100 }, Vector(50, 50), drawSin);
-	CircleButton cosButton ("COS",   Point{ 300, 100 }, Vector(50, 50), drawCos);
-	OvalButton   tanButton ("TAN",   Point{ 500, 100 }, Vector(50, 50), drawTan);
-	CloseButton  clrButton ("CLEAR", Point{ 700, 100 }, Vector(50, 50), clear  );
+	Button		 sinButton ("SIN",   Point{ 100, 100 }, Vector( 50, 50 ), drawSin);
+	CircleButton cosButton ("COS",   Point{ 300, 100 }, Vector( 50, 50 ), drawCos);
+	OvalButton   tanButton ("TAN",   Point{ 500, 100 }, Vector( 50, 50 ), drawTan);
+	CloseButton  clrButton ("CLEAR", Point{ 700, 100 }, Vector( 50, 50 ), clear  );
 
 	Button*       buttons[count] { &sinButton, &cosButton, &tanButton, &clrButton };
 	ButtonManager manager	     ( buttons, count );
@@ -68,3 +69,5 @@ int main()
 		txSleep();
 	}
 }
+
+//TODO: Класс менеджера кнопок, добавить функцию добавления кнопки в массив указателей кнопок (добавлять в динамический массив или большой статический).
